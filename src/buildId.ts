cb6a5312ad08d5c91af025c0f03641ba0a1c7866
Gabriel Chaves <slgabriel.chaves@gmail.com>
2025-12-28 17:09:49 -0300

    chore: add build ID to force Service Worker cache bust

---
+++ src/buildId.ts
@@ -0,0 +1,4 @@
+// Force Service Worker update
+// This file changes with each deployment to bust SW cache
+export const BUILD_ID = 'v0.1.0-20251228-1710';
+export const DEPLOYMENT_TIME = new Date('2025-12-28T17:10:00-03:00').getTime();
